585cfe961c759a7dec45bc4ff4d37c9ef15f0095
Lekan <olalekancrown11@gmail.com>
2022-04-01 22:30:03 +0100

    Add kyu folder and new solution

---
+++ 7-Kyu/SumOfNumbersFrom0ToN.js
@@ -0,0 +1,18 @@
+// 01-04-2022
+// Fundamentals
+var SequenceSum = (function() {
+    function SequenceSum() {}
+  
+    SequenceSum.showSequence = function(count) {
+      let arr=[];
+      for(let i=0;i<=count;i++) {arr.push(i)};
+      let b=arr.reduce((total,next)=>total+next,0)
+      let a=arr.join('+');
+      if(count<0) return `${count}<0`;
+      if(count==0) return `${count}=0`;
+      return `${a} = ${b}`
+    };
+  
+    return SequenceSum;
+  
+  })();
--- app.js
+++
@@ -1,4 +0,0 @@
-// Disemvowel Trolls
-function disemvowel(str) {
-  return str.replace(/a|e|i|o|u/gi, "");
-}
