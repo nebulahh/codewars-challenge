628da9c0ad0e9445edb17d98e515f992cfd389e1
Lekan <olalekancrown11@gmail.com>
2022-04-06 17:19:08 +0100

    Solve codewars

---
+++ 7-Kyu-Fundamentals/05-04-22.js
@@ -0,0 +1,10 @@
+// Find the capitals
+var capitals = function (word) {
+  var ara = [];
+  for (var i = 0; i < word.length; i++) {
+    if (word[i] == word[i].toUpperCase()) {
+      ara.push(i);
+    }
+  }
+  return ara;
+};
