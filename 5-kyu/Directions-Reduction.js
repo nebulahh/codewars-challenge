202754c29ae9e626a6d9cf1a3928082587d46489
nebulahh <94326584+nebulahh@users.noreply.github.com>
2022-06-20 09:04:14 +0100

    Add direction reduction file

---
+++ 5-kyu/Directions-Reduction.js
@@ -0,0 +1,16 @@
+function dirReduc(arr){
+    const opposites = {'NORTH': 'SOUTH', 'EAST': 'WEST', 'SOUTH': 'NORTH', 'WEST': 'EAST'};
+    const result = []
+    arr.forEach(item => {
+        if(result.length) {
+            const prevItem = result.pop()
+            if(prevItem !== opposites[item]) {
+                result.push(prevItem)
+                result.push(item)
+            }
+        } else {
+            result.push(item)
+        }
+    })
+    return result
+}
