81d9d4077e5416f6a181f03a02204b5fa6e16e35
nebulahh <94326584+nebulahh@users.noreply.github.com>
2022-06-22 07:59:06 -0400

    Add solution

---
+++ 7-Kyu-Fundamentals/number-of-people-in-the-bus.js
@@ -0,0 +1,11 @@
+var number = function(busStops){
+  let peopleIn = 0;
+  let peopleOut = 0;
+  
+  for (let i = 0; i < busStops.length; i++) {
+    peopleIn += busStops[i][0];
+    peopleOut += busStops[i][1];
+  }
+  
+  return peopleIn - peopleOut;
+}
