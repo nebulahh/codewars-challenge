81530a38e8d32801770f11aa1551dba711908e07
nebulahh <94326584+nebulahh@users.noreply.github.com>
2022-06-25 16:53:18 +0100

    Add product of consecutive fib numbers file

---
+++ 5-kyu/product-of-consecutive-fib-numbers.js
@@ -0,0 +1,11 @@
+function productFib(prod){
+  let n = 0;
+  let nPlus = 1;
+  
+  while (n * nPlus < prod) {
+    nPlus = n + nPlus;
+    n = nPlus - n;
+  }
+  
+  return [n, nPlus, n * nPlus === prod];
+}
