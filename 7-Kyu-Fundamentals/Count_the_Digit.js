db8c5ccbdf9dc6c656b61fd7ac463faf8e1b5086
nebulahh <94326584+nebulahh@users.noreply.github.com>
2022-08-14 13:38:57 +0100

    Add solution

---
+++ 7-Kyu-Fundamentals/Count_the_Digit.js
@@ -0,0 +1,16 @@
+function nbDig(n, d) {
+  var numberMatches = 0;
+
+  for(var i = 0; i <= n; i++) {
+    var squared = i*i;
+    var squaredString = squared.toString();
+    
+    for(var k = 0; k<=squaredString.length; k++) {
+      if(squaredString[k] === d.toString()) {
+      numberMatches += 1;
+      }
+    } 
+    
+  }
+  return numberMatches;
+}
