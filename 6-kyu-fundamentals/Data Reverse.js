c0837ea32d57a8c247011969c3e8532c18efcfe0
nebulahh <94326584+nebulahh@users.noreply.github.com>
2022-11-14 23:10:47 +0100

    Create Data Reverse.js

---
+++ 6-kyu-fundamentals/Data Reverse.js
@@ -0,0 +1,9 @@
+function dataReverse(data) {
+    if (data.length===0) return []
+  const arrayBytes = [];
+  const tam = data.length / 8;
+  for (var i = 0; i < tam; i++){
+    arrayBytes.push(data.splice(0,8));
+  }
+  return arrayBytes.reverse().join().split(',').map(v=>v*1);
+}
